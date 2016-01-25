feb1a37784ba04ca013f46a94ab90edbb1bafb09
Remi DAVID <rdavid@e1r8p8.42.fr>
2016-01-25 10:24:35 +0100

    fix virtual function prototype

--- inc/Octree.hpp
+++ inc/Octree.hpp
@@ -27,7 +27,7 @@ public:
 	virtual Octree *	insert(float const &, float const &, float const &, uint32_t const &, int32_t const &) { return (NULL); }
 	virtual Octree *	search(float const &, float const &, float const &) { return (NULL); }
 	virtual Octree *	search(float const &, float const &, float const &, int const &) { return (NULL); }
-	virtual void		render(Core &core) {(void)core;}
+	virtual void		render(Core &core) const {(void)core;}
 	void				setCube(float const &x, float const &y, float const &z, float const &s);
 	void				setState(int32_t const &state);
 	void				setParent(Octree *parent);
